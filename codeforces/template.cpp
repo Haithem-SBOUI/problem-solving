1e2f44b726356a7f51ee57fee789fa7c07eff1e7
Haithem-SB <haithem.sb999@gmail.com>
2025-04-18 18:25:21 +0200

    init repo

---
+++ codeforces/template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+typedef long long ll;
+const int N = 3e5 + 7;
+
+
+void test_case() {
+
+}
+
+int main() {
+    fastio;
+    int T=1;
+    // cin >> T;
+    while (T--) {
+        test_case();
+    }
+    return 0;
+}
